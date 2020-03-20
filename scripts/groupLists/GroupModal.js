919627c1f9cdbb34b4d0cca5e2d9140a5a2a43db
EMILS <emils.bisenieks93@gmail.com>
2020-03-20 21:44:57 +0200

    modal - DONE

--- scripts/groupLists/GroupModal.js
+++ scripts/groupLists/GroupModal.js
@@ -1,7 +1,38 @@
+import { convertDatoToAge } from '../utils/utils.js';
+
 export function groupModal(modal, modalData) {
   modal.classList.add('modal-open');
   const innerModal = modal.firstElementChild;
   innerModal.innerHTML = `
-    <div>${modalData.first_name} ${modalData.last_name} ${modalData.birth_date} ${modalData.gender} ${modalData.mothers_name} ${modalData.mothers_phone} ${modalData.fathers_name} ${modalData.fathers_phone} ${modalData.notes}</div>
+    <section class="modal-child-info">
+      <p>${modalData.first_name} ${modalData.last_name}</p>
+      <p><i class="fas fa-birthday-cake"></i>${modalData.birth_date}</p>
+      <p><i class="far fa-calendar-alt"></i>${convertDatoToAge(
+        modalData.birth_date
+      )}</p>
+    </section>
+    <section class="modal-parents-info">
+      <div class="modal-mother-info">
+        <p>Mothers name: ${modalData.mothers_name}</p>
+        <p><i class="fas fa-phone"></i>${modalData.mothers_phone}</p></div>
+      <div class="modal-father-info">
+        <p>Fathers name: ${modalData.fathers_name}</p>
+        <p><i class="fas fa-phone"></i>${modalData.fathers_phone}</p>
+      </div>
+    </sections>
+    <section class="modal-child-notes">
+      <p><i class="fas fa-sticky-note"></i>${modalData.notes}</p>
+    </sections>
   `;
+  modalClose(modal, innerModal);
+}
+
+function modalClose(modal, innerModal) {
+  const closeModalEl = document.createElement('div');
+  closeModalEl.classList.add('modal-close');
+  closeModalEl.textContent = 'X';
+  closeModalEl.addEventListener('click', () => {
+    modal.classList.remove('modal-open');
+  });
+  innerModal.insertAdjacentElement('beforeend', closeModalEl);
 }
