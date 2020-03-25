c0cadabbad17aa3dbf9eed0527bd13be0480c25f
EMILS <emils.bisenieks93@gmail.com>
2020-03-25 19:31:42 +0200

    Converting input items to json object

--- scripts/waitingLists/waitingList.js
+++ scripts/waitingLists/waitingList.js
@@ -2,38 +2,47 @@ import { modalClose } from '../groupLists/GroupModal.js';
 
 const addItem = document.querySelector('.wl-add-item');
 const formModal = document.querySelector('.modal');
+const innerFormModal = formModal.firstElementChild;
+const formSubmit = document.querySelector('.as-form');
+console.log([...formSubmit.children]);
+
+function getKeyValuesPairs(form) {
+  const inputsArray = [...form.children];
+  const paraisArray = inputsArray
+    .filter(input => {
+      if (input.type === 'text' || input.type === 'tel') {
+        return input;
+      }
+    })
+    .map(input => {
+      return {input.name};
+    });
+  console.log(paraisArray);
+}
+
+getKeyValuesPairs(formSubmit);
+
+function formSubmitData(e) {
+  e.preventDefault();
+  console.log(e.currentTarget);
+
+  // const formData = new FormData(this);
+  // console.log(formData);
+
+  // const formPostData = fetch('http://localhost:3000/waiting-list', {
+  //   method: 'post',
+  //   body: formData,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
+}
+
+formSubmit.addEventListener('submit', formSubmitData);
 
 function openForm() {
   formModal.classList.add('modal-open');
-  const innerFormModal = formModal.firstElementChild;
-  innerFormModal.innerHTML = `
-    <form class='as-form'>  
-      <h4>ADD STUDENT TO WAITING LIST</h4>
-      <div class='form-child-info'>
-        <input type='text' name='first_name' placeholder='Name'>
-        <input type='text' name='last_name' placeholder='Last Name'>
-        <input type='text' name='birthday' placeholder='Birthday'>
-      </div>
-      <div class='form-parent-info'>
-        <div class='fp-mother'>
-          <input type='text' name='mother_name' placeholder='Mothers Name'>
-          <input type='tel' name='mother_phone' placeholder='Mothers Phone Number' pattern='[0-9]'>
-        </div>
-        <div class='fp-father'>
-          <input type='text' name='father_name' placeholder='Fathers Name'>
-          <input type='tel' name='father_phone' placeholder='Fathers Phone Number' pattern='[0-9]'>
-        </div>
-      </div>
-      <div class='form-notes'>
-        <textarea placeholde='Notes' rows='4' cols='50'></textarea>
-      </div>
-      <div class='form-buttons'>
-        <input type='submit' value='SUBMIT' name='form-submit'>
-      </div>
-    </form>
-  `;
   modalClose(formModal, innerFormModal);
-  // innerFormModal.insertAdjacentElement('beforeend', formEl);
 }
 
 addItem.addEventListener('click', openForm);
