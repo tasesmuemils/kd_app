1ee7ef65af1c43e41e50ecced66cf6f77e1a0c03
EMILS <emils.bisenieks93@gmail.com>
2020-03-26 23:58:12 +0200

    Creating object with form names as keys and values as values and POST to 'server'

--- scripts/waitingLists/waitingList.js
+++ scripts/waitingLists/waitingList.js
@@ -4,20 +4,23 @@ const addItem = document.querySelector('.wl-add-item');
 const formModal = document.querySelector('.modal');
 const innerFormModal = formModal.firstElementChild;
 const formSubmit = document.querySelector('.as-form');
-console.log([...formSubmit.children]);
+// console.log([...formSubmit.children]);
 
 function getKeyValuesPairs(form) {
   const inputsArray = [...form.children];
-  const paraisArray = inputsArray
+  const emptyObject = {};
+  const pairsArray = inputsArray
     .filter(input => {
       if (input.type === 'text' || input.type === 'tel') {
         return input;
       }
     })
     .map(input => {
-      return {input.name};
+      const key = input.name;
+      return (emptyObject[key] = `${input.value}`);
     });
-  console.log(paraisArray);
+  console.log(emptyObject);
+  return emptyObject;
 }
 
 getKeyValuesPairs(formSubmit);
@@ -26,16 +29,13 @@ function formSubmitData(e) {
   e.preventDefault();
   console.log(e.currentTarget);
 
-  // const formData = new FormData(this);
-  // console.log(formData);
-
-  // const formPostData = fetch('http://localhost:3000/waiting-list', {
-  //   method: 'post',
-  //   body: formData,
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // });
+  fetch('http://localhost:3000/waiting-list', {
+    method: 'post',
+    body: JSON.stringify(getKeyValuesPairs(formSubmit)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
 
 formSubmit.addEventListener('submit', formSubmitData);
