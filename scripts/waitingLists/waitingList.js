fe2e43d052b186f63947e752a7c8f1482d4b86c4
EMILS <emils.bisenieks93@gmail.com>
2020-03-29 15:44:01 +0300

    Trying to stop fetch POST from page reload

--- scripts/waitingLists/waitingList.js
+++ scripts/waitingLists/waitingList.js
@@ -1,22 +1,28 @@
 import { getKeyValuesPairs, modalClose } from '../utils/utils.js';
+import { WaitingList } from './newWaitingListTable.js';
 
 const addItem = document.querySelector('.wl-add-item');
 const formModal = document.querySelector('.modal');
 const innerFormModal = formModal.firstElementChild;
 const formSubmit = document.querySelector('.as-form');
+console.log(formSubmit);
+const wlwrapper = document.querySelector('.group-list');
 
 // Submits waiting lists form data to server
 function formSubmitData(e) {
   e.preventDefault();
-
+  console.log('NOT REFRESH');
   fetch('http://localhost:3000/waiting_list', {
-    method: 'post',
+    method: 'POST',
     body: JSON.stringify(getKeyValuesPairs(formSubmit)),
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
   formModal.classList.remove('modal-open');
+  // fetchData();
 }
 
 // Getting waiting lists data from server
@@ -26,6 +32,7 @@ async function fetchData() {
 
   const data = await response.json();
   console.log(data);
+  new WaitingList(data, wlwrapper);
 }
 fetchData();
 
