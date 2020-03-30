9b7b548f0ea1b55f85e2b10ae4b14d10b76569d5
EMILS <emils.bisenieks93@gmail.com>
2020-03-30 22:47:57 +0300

    POST method still reloads page

--- scripts/waitingLists/waitingList.js
+++ scripts/waitingLists/waitingList.js
@@ -10,36 +10,37 @@ const wlwrapper = document.querySelector('.group-list');
 
 // Submits waiting lists form data to server
 function formSubmitData(e) {
-  e.preventDefault();
-  console.log('NOT REFRESH');
-  fetch('http://localhost:3000/waiting_list', {
-    method: 'POST',
-    body: JSON.stringify(getKeyValuesPairs(formSubmit)),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then(data => console.log(data));
-  formModal.classList.remove('modal-open');
-  // fetchData();
+  // formModal.classList.remove('modal-open');
 }
 
-// Getting waiting lists data from server
-async function fetchData() {
-  const urlGroups = 'http://localhost:3000/waiting_list';
-  const response = await fetch(urlGroups);
+// body: JSON.stringify(getKeyValuesPairs(formSubmit)),
+// headers: {
+//   // 'Content-Type': 'application/json',
+// },
 
-  const data = await response.json();
-  console.log(data);
-  new WaitingList(data, wlwrapper);
-}
-fetchData();
+// // Getting waiting lists data from server
+// async function fetchData() {
+//   const urlGroups = 'http://localhost:3000/waiting_list';
+//   const response = await fetch(urlGroups);
+
+//   const data = await response.json();
+//   console.log(data);
+//   new WaitingList(data, wlwrapper);
+// }
+// // fetchData();
 
 function openForm() {
   formModal.classList.add('modal-open');
   modalClose(formModal, innerFormModal);
 }
 
-formSubmit.addEventListener('submit', formSubmitData);
-addItem.addEventListener('click', openForm);
+// formSubmit.addEventListener('click', );
+addItem.addEventListener('click', e => {
+  e.preventDefault();
+  console.log();
+  const url = 'http://localhost:3000/waiting_list';
+  const options = { method: 'POST' };
+  fetch(url, options);
+  console.log('Still not reloading');
+  formModal.classList.remove('modal-open');
+});
